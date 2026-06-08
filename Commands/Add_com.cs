a3f2c481a5d9e38cebc592a89ff40e1944acaaa0
Eternally16 <eternally1615@gmail.com>
2026-06-08 16:20:47 +0300

    I finished the parsing and added saving to the database

--- Commands/Add_com.cs
+++ Commands/Add_com.cs
@@ -1,35 +1,45 @@
 
+using System.Xml.Linq;
 using Microsoft.Data.Sqlite;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+
 
 namespace TelegramBotApp
 {
     public class Add_com : IBotCommand
     {
-        // Привязываем класс конкретно к этой команде
+        // Command class
         public string Name => "/add";
 
+        // Function
         public async Task ExecuteAsync(ITelegramBotClient client, Update update)
         {
 
             long chatId = update.Message?.Chat.Id ?? 0;
             string? messageText = update.Message?.Text;
         
+        // Parsing
+        try {
+        
         if (messageText.StartsWith("/add")) {
         
         // Parsing command
         string parsing0 = messageText.Substring(5);
-
         int spaceIndex = parsing0.IndexOf(' ');
-        
-        
+
         string date = parsing0.Substring(0,spaceIndex);
         string text = parsing0.Substring(spaceIndex + 1);
 
-         await client.SendMessage(chatId,$"DATA: {date}" + $"TEXT: {text}");
+        // Parsing protaction
 
+        if (!DateTime.TryParse(date,out DateTime remindDate))
+        {
+            await client.SendMessage(chatId,"❌ Неверны формат нужен: ГГГГ.ММ.ДД");
+            return;
         }
+
+        await client.SendMessage(chatId,$"DATA: {date}" + $"TEXT: {text}");
 
         // DB Path
         string connectDB = "Data Source=reminders.db";
@@ -39,16 +49,27 @@ namespace TelegramBotApp
             using (var connect = new SqliteConnection(connectDB))
             {
                 connect.Open();
+                var write = connect.CreateCommand();
+                write.CommandText = @"
+                    INSERT INTO reminders (chat_id,text,remind_date)
+                    VALUE (@chatid, @text,@date)
+                ";
+                write.Parameters.AddWithValue("@chatid",chatId);
+                write.Parameters.AddWithValue("@text",text);
+                write.Parameters.AddWithValue("@date",date);
 
-
-
-
+                await client.SendMessage(chatId,$"Напоминание сохранено !!!");
             }
-        } catch {
-
-            
+        } catch (Exception ex) {
+            await client.SendMessage(chatId,$"❌ Ошибка сохранения базы данных!! {ex}");
         }
 
+
+        }
+
+        } catch {
+            await client.SendMessage(chatId,"❌ Неверный формат: /add ГГГГ.ММ.ДД ТЕКСТ");
+        }
 
         }
     }
